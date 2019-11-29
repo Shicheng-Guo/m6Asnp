faa7ee9cb811109dda2eb4d868e29ca209d52eb0
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-28 23:52:24 -0600

    Update run.R

--- extdata/alzheimer/run.R
+++ extdata/alzheimer/run.R
@@ -37,6 +37,15 @@ snp<-unique(snp)
 input2<-unique(data[data$MarkerName %in% snp,])
 write.table(input2,file="m6A-GWAS-hit-alzheimer.stage2.20191127.txt",sep="\t",quote=F,col.names=NA,row.names = T)
 
+## manhattan plot and qq-plot
+library("CMplot")
+setwd("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/m6A")
+P1<-read.table("m6A-GWAS-hit-alzheimer.stage1.20191127.txt",head=T)
+P2<-read.table("m6A-GWAS-hit-alzheimer.stage2.20191127.txt",head=T)
+P<-rbind(P1,P2)
+head(P)
+cminput<-data.frame(SNP=P$MarkerName,Chromosome=P$Chromosome,Position=P$Position,trait1=P$Pvalue)
+CMplot(cminput,plot.type="b",ylim=20,LOG10=TRUE,threshold=NULL,file="jpg",memo="",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
 
 
 
