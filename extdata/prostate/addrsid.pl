4eeab175b320f2f7a5516111429d88fe3d87ed14
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-29 20:15:00 -0600

    Create addrsid.pl

---
+++ extdata/prostate/addrsid.pl
@@ -0,0 +1,33 @@
+
+use strict;
+use Cwd;
+my $dbsnp153="dbSNP153.hg19.vcf";
+my %data;
+#open F1,'<:gzip',$dbsnp153 || die "cannot open $dbsnp153";
+open F1,$dbsnp153 || die "cannot open $dbsnp153";
+while(<F1>){
+next if /^#/;
+my ($chr,$pos,$rs,$ref,$alt)=split/\s+/;
+$ref=uc $ref;
+$alt= uc $alt;
+my $id="$chr-$pos-$ref-$alt";
+$data{$id}=$rs;
+}
+
+my $input=shift @ARGV;
+open F2, $input;
+while(<F2>){
+my @line=split/\s+/;
+my $chr=$line[3];
+my $pos=$line[4];
+my $ref=uc $line[6];
+my $alt=uc $line[5];
+my $id="$chr-$pos-$ref-$alt";
+if(defined $data{$id}){
+$line[2]=$data{$id};
+$line[5]=uc $line[5];
+$line[6]=uc $line[6];
+}
+my $print= join("\t",@line);
+print "$print\n";
+}
