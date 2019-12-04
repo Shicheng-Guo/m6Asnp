5650e294ed03115ec41910388257d39e9c6db829
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-12-03 23:43:15 -0600

    Update meta2m6A.R

--- extdata/breast/meta2m6A.R
+++ extdata/breast/meta2m6A.R
@@ -1,10 +1,14 @@
+ 
 args = commandArgs(trailingOnly=TRUE)
 
 meta.file=as.character(args[1])
 output.file=as.character(args[2])
 
+# meta.file="oncoarray_bcac_public_release_oct17.chr22.rsid.txt"
+# output.file="oncoarray_bcac_public_release_oct17.chr22.m6A.txt"
+ 
 input<-read.table(meta.file,head=T,sep="\t")
-table<-data.frame(table,OR=exp(table$bcac_onco_icogs_gwas_beta))
+table<-data.frame(input,OR=exp(as.numeric(input$bcac_onco_icogs_gwas_beta)))
 db<-read.table("~/hpc/db/hg19/m6ASNP.db147.hg19.uni.txt",head=T)
 head(db)
 newdb<-db[db$Rs_ID %in% table$phase3_1kg_id,]
